04b8aa0c11ac4bb13033b744a7472ed006c1dc55
Luke Collins <luke23571113@gmail.com>
2020-10-24 11:31:34 -0700

    initial upload

---
+++ Skip List Project/NOde.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skip_List_Project
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public List<Node<T>> Neighbors { get; set; }
+
+        public Node<T> this[int index]
+        {
+            get
+            {
+                return Neighbors[index];
+            }
+            set
+            {
+                Neighbors[index] = value;
+            }
+        }
+
+        public Node(int height = 1)
+        {
+            Neighbors = new List<Node<T>>(height);
+        }
+
+        public Node(T val, int height) : this(height)
+        {
+            Value = val;
+        }
+    }
+}
---
+++ Skip List Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skip_List_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
